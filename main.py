3e34e68ec9aa7ba3f7f9dcdb59c548ed13f9be04
Sara-Xavier <saraaxavierrr@gmail.com>
2024-08-21 21:17:55 -0300

    Primeiras configurações de tela e cores

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame # Biblioteca padrão de games
+import random # Usado posteriormente para deixar o lugar da comida da cobra aleatório
+
+pygame.init()
+
+######## Cores ########################
+branco = (255, 255, 255)
+preto = (0, 0, 0,)
+vermelho = (213, 50, 80)
+verde = (0, 255, 0)
+######## Cores ########################
+
+######## Confugurações da tela ########################
+largura = 600
+altura = 400
+tela = pygame.display.set_caption('Mestre das Serpentes')
+######## Confugurações da tela ########################
